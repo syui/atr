47eae25ab24f3fd8fb8c02e3ccd6c5d60ea70439
syui <syui@syui.ai>
2023-04-03 19:18:16 +0900

    fix api null

--- scpt/api_card.zsh
+++ scpt/api_card.zsh
@@ -9,19 +9,21 @@ d=`date +"%Y%m%d"`
 nd=`date +"%Y%m%d" -d '1 day'`
 username=`echo $1|cut -d . -f 1`
 url=https://api.syui.ai
+url_user_all="$url/users?itemsPerPage=255"
 f=$HOME/.config/atr/scpt/t.webp
 
 if [ -z "$1" ];then
 	exit
 fi
 
-data=`curl -sL $url/users|jq ".[]|select(.username == \"$username\")"`
+data=`curl -sL $url_user_all|jq ".[]|select(.username == \"$username\")"`
 if [ -z "$data" ];then
 	data=`curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"$username\"}" -s $url/users`
 	echo $data|jq -r .username
 fi
 next=`echo $data|jq -r .next`
-if [ $next -gt $d ];then
+
+if [ $next -gt $d ] && [ "$2" != "--thx" ];then
 	echo limit 1 day
 	echo "next : $nd"
 	t=0
@@ -40,7 +42,6 @@ t=`echo $tmp|jq -r .card`
 tmp=`curl -X PATCH -H "Content-Type: application/json" -d "{\"next\":\"$nd\"}" -s $url/users/$uid`
 next=`echo $tmp|jq -r .next`
 echo next : $next
-echo url : $card_url
 
 f=$HOME/.config/atr/scpt/t.webp
 curl -sL -o $f https://card.syui.ai/card/card_${t}.webp
