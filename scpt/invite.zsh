e151be3487d44cb6044e6b290855b075fa0277b3
syui <syui@syui.ai>
2023-04-20 13:02:04 +0900

    add invite

---
+++ scpt/invite.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+admin_password=`cat $HOME/.config/atr/api_card.json|jq -r .pds_admin_password`
+host=bsky.syui.ai
+url=https://$host/xrpc/com.atproto.server.createInviteCode
+json="{\"useCount\":2}"
+curl -X POST -u admin:${admin_password} -H "Content-Type: application/json" -d "$json" -sL $url | jq -r .code
